80f84c09f789fda2d68fa6660ddcb9e8746cabca
zen123 <zhiyuan0312@hotmail.com>
2023-09-11 18:03:53 +0800

    ipfs upload + mint nft

--- final-project/final-project/frontend/components/board/index.tsx
+++ final-project/final-project/frontend/components/board/index.tsx
@@ -1,27 +1,73 @@
 import { useState } from "react";
+import pinataSDK from "@pinata/sdk";
+import { useAccount, useContractWrite } from "wagmi";
+import * as etherBoardJson from "./EtherBoard.json";
 
 export default function Board() {
   const [message, setMessage] = useState<string>("");
+  const [cid, setCid] = useState<string | null>(null);
+  const [ipfsstatusMessage, setipfsStatusMessage] = useState<string | null>(
+    null
+  );
+  const { address } = useAccount();
+  const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_API_KEY;
+  const PINATA_API_SECRET = process.env.NEXT_PUBLIC_PINATA_API_SECRET;
+  const pinata = new pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+  const ETHERBOARD_ADDRESS = "0x51D1a7B8Cf3523E83C78d2f10F63eB855Fa8Eb5a";
 
-  function handleMessage(newMessage: string) {
-    setMessage(newMessage);
+  const { write, isLoading, isSuccess, data } = useContractWrite({
+    address: ETHERBOARD_ADDRESS,
+    abi: etherBoardJson.abi,
+    functionName: "safeMint",
+  });
+
+  async function handleMint() {
+    setipfsStatusMessage("Uploading to IPFS...");
+
+    const metadata = {
+      message: message,
+    };
+
+    try {
+      const result = await pinata.pinJSONToIPFS(metadata);
+      setipfsStatusMessage(
+        `Successfully uploaded to IPFS with CID: ${result.IpfsHash}`
+      );
+      setCid(result.IpfsHash);
+
+      console.log("Initiating NFT Minting...");
+      const nftURI = `https://ipfs.io/ipfs/${result.IpfsHash}`;
+      if (!address) {
+        console.error("Account is not defined");
+        return;
+      }
+      write({
+        args: [address, nftURI, message],
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 
   return (
     <div>
-      {/* input */}
       <textarea
         className="block border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue w-full sm:w-1/2"
         rows={4}
         placeholder="Type your message here!"
         value={message}
-        onChange={(e) => handleMessage(e.target.value)}
+        onChange={(e) => setMessage(e.target.value)}
       />
-      <button className="mt-4 bg-purple hover:bg-dark-purple text-white font-bold py-2 px-4 rounded">
+      <button
+        className="mt-4 bg-purple hover:bg-dark-purple text-white font-bold py-2 px-4 rounded"
+        onClick={handleMint}
+        disabled={!message || !address || isLoading}
+      >
         Mint Message
       </button>
-
-      {/* board */}
+      <div className="mt-2 text-gray-600">{ipfsstatusMessage}</div>
+      {isLoading && <div>Minting...</div>}
+      {isSuccess && <div>Minted successfully: {JSON.stringify(data)}</div>}
       <hr className="border-1 rounded my-8 border-blue" />
     </div>
   );
